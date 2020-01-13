84bdf1c7641996e5ec90c60c422daae52d4145ae
KevinLvlssNabot <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-01-13 12:02:20 +0100

    AjoutDeFinDeCombat
    Le combat se termine lorsque l'ennemi arrive à 0 PV.

--- TP1.c
+++ TP1.c
@@ -4,5 +4,22 @@
 
 int main(){
 
+  int pvGuts = 50;
+  int pvMonstre = 50;
+  int attaqueGuts = 5;
+
+while (pvMonstre != 0) {
+  printf("Guts attaque !\n");
+  printf("Le monstre possede %d PV.\n", pvMonstre);
+  printf("Le monstre subit %d degats\n", attaqueGuts);
+  pvMonstre = pvMonstre-attaqueGuts;
+  printf("Il reste %d PV au monstre !\n", pvMonstre);
+}
+  //  printf("Guts attaque !\n");
+  //  printf("Le monstre possede %d\n", pvMonstre);
+  //  printf("Le monstre subit %d degats\n", attaqueGuts);
+  //  pvMonstre = pvMonstre-attaqueGuts;
+  //  printf("Il reste %d PV au monstre !\n", pvMonstre);
+
 return 0;
 }
