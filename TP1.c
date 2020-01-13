586f8370cc38dabb77f265d3d1049b5e01b54b9c
NabotKevin <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-01-13 12:58:04 +0100

    Update TP1.c

--- TP1.c
+++ TP1.c
@@ -7,23 +7,7 @@ int main(){
   int pvGuts = 50;
   int pvMonstre = 50;
   int attaqueGuts = 5;
-
-
-while (pvMonstre != 0) {
-  printf("Guts attaque !\n");
-  printf("Le monstre possede %d PV.\n", pvMonstre);
-  printf("Le monstre subit %d degats\n", attaqueGuts);
-  pvMonstre = pvMonstre-attaqueGuts;
-  printf("Il reste %d PV au monstre !\n", pvMonstre);
-}
-  //  printf("Guts attaque !\n");
-  //  printf("Le monstre possede %d\n", pvMonstre);
-  //  printf("Le monstre subit %d degats\n", attaqueGuts);
-  //  pvMonstre = pvMonstre-attaqueGuts;
-  //  printf("Il reste %d PV au monstre !\n", pvMonstre);
-=======
   int attaqueMonstre = 4;
-
   int attaqueOuDefense;
 
     while (pvMonstre > 0 && pvGuts > 0) {
