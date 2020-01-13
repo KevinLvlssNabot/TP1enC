56445c4996919022b97cc90528f5e07d0b542309
KevinLvlssNabot <59653290+KevinLvlssNabot@users.noreply.github.com>
2020-01-13 11:51:16 +0100

    SystèmeDePvDeBase
    Ajout du système de pv.

--- TP1.c
+++ TP1.c
@@ -4,5 +4,19 @@
 
 int main(){
 
+  int pvGuts = 50;
+  int pvMonstre = 50;
+  int attaqueGuts = 5;
+
+    printf("Guts attaque !\n");
+    printf("Le monstre possede %d\n", pvMonstre);
+    printf("Le monstre subit %d degats\n", attaqueGuts);
+    pvMonstre = pvMonstre-attaqueGuts;
+    printf("Il reste %d PV au monstre !\n", pvMonstre);
+
+
+
+
+
 return 0;
 }
